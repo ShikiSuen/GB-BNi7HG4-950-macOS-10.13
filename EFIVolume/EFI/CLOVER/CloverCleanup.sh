b816f58682fe15e1478756070238a6c8c9bda1ab
ShikiSuen <shikisuen@outlook.com>
2018-07-11 10:44:54 +0800

    macOS 10.13.6 (17G65)

---
+++ EFIVolume/EFI/CLOVER/CloverCleanup.sh
@@ -0,0 +1,17 @@
+rm -rf ._config.plist
+rm -rf ACPI
+rm -rf drivers64UEFI/._apfs.efi
+rm -rf kexts/10.10
+rm -rf kexts/10.11
+rm -rf kexts/10.12
+rm -rf kexts/10.13
+rm -rf kexts/10.6
+rm -rf kexts/10.7
+rm -rf kexts/10.8
+rm -rf kexts/10.9
+rm -rf kexts/Other/._FakeSMC.kext
+rm -rf kexts/Other/._IntelMausiEthernet.kext
+rm -rf kexts/Other/._USBInjectAll.kext
+rm -rf misc
+rm -rf OEM
+rm -rf ROM
